9c0bb932b5d69c27d18a5056d013f91acfe82c60
Yang Zhang <t-yaz@microsoft.com>
2014-12-11 18:08:49 +0800

    test init

---
+++ src/test/java/com/microsoft/CAPS/util/WebUICommonTestHelper.java
@@ -0,0 +1,14 @@
+package com.microsoft.CAPS.util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * Created by t-yaz on 12/11/2014.
+ */
+public class WebUICommonTestHelper {
+    protected WebDriver driver;
+    protected String baseURL;
+    protected WebDriverWait waiter;
+
+}
